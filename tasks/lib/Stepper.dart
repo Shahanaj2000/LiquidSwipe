978919f580967dbb97aac31b1f277bd83db70e31
Shahanaj2000 <shahanajmt@gmail.com>
2023-01-25 23:05:20 +0530

    CONTINUE & CANCEL Button Customize using ControllerBuilder properties

--- tasks/lib/Stepper.dart
+++ tasks/lib/Stepper.dart
@@ -36,10 +36,26 @@ class _MyStepperState extends State<MyStepper> {
   }
 
   //! Directly gives us the index of tap steps
-  onTapped( int value) {
+  onTapped(int value) {
     setState(() {
       currentIndex = value;
     });
+  }
+
+  Widget controlBuilders(context, details) {
+    return Row(
+      children: [
+        ElevatedButton(
+          onPressed: details.onStepContinue,
+          child: const Text("Next"),
+        ),
+        const SizedBox(width: 10,),
+        OutlinedButton(
+          onPressed:  details.onStepCancel,
+          child: const Text("Back"),
+        )
+      ],
+    );
   }
 
   @override
@@ -50,24 +66,44 @@ class _MyStepperState extends State<MyStepper> {
         onStepContinue: continueStep,
         onStepCancel: cancelStep,
         onStepTapped: onTapped,
+        controlsBuilder: controlBuilders,
+        type: StepperType.horizontal,
+        elevation: 0,
         steps: [
           Step(
             title: const Text('Step 1'),
             content: Container(
                 alignment: Alignment.centerLeft,
-                child: const Text("This is the step 1")),
+                child: const Padding(
+                  padding:  EdgeInsets.all(8.0),
+                  child:  Text("This is the step 1"),
+                )),
+                isActive: currentIndex >= 0,
+                state: currentIndex >= 0 ? StepState.complete : StepState.disabled,
+                // subtitle: Text("Hai"),
+                // label: Icon(Icons.check)
           ),
           Step(
             title: const Text('Step 2'),
             content: Container(
                 alignment: Alignment.centerLeft,
-                child: const Text("This is the step 2")),
+                child: const Padding(
+                  padding:  EdgeInsets.all(8.0),
+                  child:  Text("This is the step 2"),
+                )),
+                isActive: currentIndex >= 1,
+                state: currentIndex >= 1 ? StepState.complete : StepState.disabled,
           ),
           Step(
             title: const Text('Step 3'),
             content: Container(
                 alignment: Alignment.centerLeft,
-                child: const Text("This is the step 3")),
+                child: const Padding(
+                  padding:  EdgeInsets.all(8.0),
+                  child:  Text("This is the step 3"),
+                )),
+                isActive: currentIndex >= 2,
+                state: currentIndex >= 2 ? StepState.complete : StepState.disabled,
           ),
         ],
       ),
